1b92f7b486d8989d553bd2ee2af701f1c9b0f933
Stijn Lingmont <stijn@stijnlingmont.nl>
2019-11-22 17:48:35 +0100

    homepage index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,10 +5,21 @@
 @endsection
 
 @section('content')
-    <div class="container">
-        <section class="section">
-            <h1>Hello World</h1>
-        </section>
-
-    </div>
+    <section class="page-header_home">
+{{--        <h1>--}}
+{{--            <span>Explore the world</span>--}}
+{{--            <span>“In photography there is a reality so subtle that it becomes more real than reality.”</span>--}}
+{{--        </h1>--}}
+    </section>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores aspernatur aut commodi consequuntur, debitis dolore ducimus, excepturi magnam minus nemo nobis pariatur perferendis, saepe sapiente sunt totam ut vero.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores aspernatur aut commodi consequuntur, debitis dolore ducimus, excepturi magnam minus nemo nobis pariatur perferendis, saepe sapiente sunt totam ut vero.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores aspernatur aut commodi consequuntur, debitis dolore ducimus, excepturi magnam minus nemo nobis pariatur perferendis, saepe sapiente sunt totam ut vero.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores aspernatur aut commodi consequuntur, debitis dolore ducimus, excepturi magnam minus nemo nobis pariatur perferendis, saepe sapiente sunt totam ut vero.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores aspernatur aut commodi consequuntur, debitis dolore ducimus, excepturi magnam minus nemo nobis pariatur perferendis, saepe sapiente sunt totam ut vero.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores aspernatur aut commodi consequuntur, debitis dolore ducimus, excepturi magnam minus nemo nobis pariatur perferendis, saepe sapiente sunt totam ut vero.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores aspernatur aut commodi consequuntur, debitis dolore ducimus, excepturi magnam minus nemo nobis pariatur perferendis, saepe sapiente sunt totam ut vero.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores aspernatur aut commodi consequuntur, debitis dolore ducimus, excepturi magnam minus nemo nobis pariatur perferendis, saepe sapiente sunt totam ut vero.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores aspernatur aut commodi consequuntur, debitis dolore ducimus, excepturi magnam minus nemo nobis pariatur perferendis, saepe sapiente sunt totam ut vero.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores aspernatur aut commodi consequuntur, debitis dolore ducimus, excepturi magnam minus nemo nobis pariatur perferendis, saepe sapiente sunt totam ut vero.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores aspernatur aut commodi consequuntur, debitis dolore ducimus, excepturi magnam minus nemo nobis pariatur perferendis, saepe sapiente sunt totam ut vero.</p>
 @endsection
