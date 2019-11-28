00400d0374f6c24dcea06ed8e80a3a14f4fdaf92
Stijn Lingmont <80831@rocteraa-student.nl>
2019-11-28 20:54:09 +0100

    Login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,45 +6,47 @@
         <form class="forum" method="POST" action="{{ route('login') }}">
             @csrf
             <div class="forum-item">
-                <input placeholder="E-mail" class="forum-input @error('email') forum-invalid @enderror" id="email" type="email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                <label for="email">Email</label>
+                <input id="email" class="forum-input @error('email') forum-invalid @enderror" id="email" type="email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
         
                 @error('email')
-                    <span>
+                    <span class="text-error">
                         <strong>{{ $message }}</strong>
                     </span>
                 @enderror
             </div>
 
             <div class="forum-item">
-                <input placeholder="Password" class="forum-input @error('password') forum-invalid @enderror" id="password" type="password" name="password" required autocomplete="current-password">
+                <label for="password">Password</label>
+                <input id="password" class="forum-input @error('password') forum-invalid @enderror" id="password" type="password" name="password" required autocomplete="current-password">
         
                 @error('password')
-                    <span>
+                    <span class="text-error">
                         <strong>{{ $message }}</strong>
                     </span>
                 @enderror
             </div>
-        
+
             <div class="forum-item">
                 <input class="forum-checkbox" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-        
+    
                 <label for="remember">
                     {{ __('Remember Me') }}
                 </label>
-            </div class="forum-item">
 
-            <div class="forum-item">
-                <button class="forum-submit" type="submit">
+                <button class="forum-submit btn btn-primary" type="submit">
                     Login
                 </button>
-        
-                @if (Route::has('password.request'))
-                    <a class="forum-button" href="{{ route('password.request') }}">
-                        Forgot Your Password?
-                    </a>
-                @endif
             </div>
         </form>
+
+        <div id="password-forgot">
+            @if (Route::has('password.request'))
+                <a class="forum-button" href="{{ route('password.request') }}">
+                    Forgot Your Password?
+                </a>
+            @endif
+        </div>
 
 
 
