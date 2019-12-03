523182111d86c6d8897b247d90764551c3a73ca4
Stijn Lingmont <stijn@stijnlingmont.nl>
2019-12-03 17:54:31 +0100

    Begin albums list

--- resources/views/albums/index.blade.php
+++ resources/views/albums/index.blade.php
@@ -5,5 +5,18 @@
 @endsection
 
 @section('content')
+    <section class="section albums">
+        <div class="container">
+            <div class="albums-head">
+                <h1>Albums</h1>
+                <a href="{{ route('album.create') }}" class="btn btn-primary">Voeg toe</a>
+            </div>
 
+            <div class="is-line"></div>
+
+            <div class="albums-list">
+
+            </div>
+        </div>
+    </section>
 @endsection
