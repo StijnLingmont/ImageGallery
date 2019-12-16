78ed66b4932a93ae416caf13f99834219e21f522
Stijn Lingmont <stijn@stijnlingmont.nl>
2019-12-16 15:35:14 +0100

    Dashboard info

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,7 +10,35 @@
                 </profile-picture>
             </div>
             <div class="is-line"></div>
-            <h1>Account</h1>
+            <div class="dashboard-body grid">
+                <div class="dashboard-profile">
+                    <h1 class="dashboard-title">Account</h1>
+                    <p class="dashboard-item"><span class="is-bold">Naam:</span> {{ auth()->user()->name }}</p>
+                    <p class="dashboard-item"><span class="is-bold">Email:</span> {{ auth()->user()->email }}</p>
+                    <p class="dashboard-item"><span class="is-bold">Created at:</span> {{ date('d-m-Y', strtotime(auth()->user()->created_at)) }}</p>
+                </div>
+                <div class="dashboard-buttons">
+                    <h1 class="dashboard-title">Dashboard</h1>
+                    <div class="dashboard-button-box">
+                        <a href="" class="dashboard-button">
+                            <img class="dashboard-button_icon" src="/images/picture.svg">
+                            <p>Albums</p>
+                        </a>
+                        <a href="" class="dashboard-button">
+                            <img class="dashboard-button_icon" src="/images/picture.svg">
+                            <p>Albums</p>
+                        </a>
+                        <a href="" class="dashboard-button">
+                            <img class="dashboard-button_icon"  src="/images/picture.svg">
+                            <p>Albums</p>
+                        </a>
+                        <a href="" class="dashboard-button">
+                            <img class="dashboard-button_icon"  src="/images/picture.svg">
+                            <p>Albums</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
