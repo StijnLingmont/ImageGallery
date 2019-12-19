09db2cc67c0fe4a802613af35750d134149c745b
Stijn Lingmont <stijn@stijnlingmont.nl>
2019-12-19 13:25:04 +0100

    Dropdown click when out of div

--- resources/js/components/Dropdown.vue
+++ resources/js/components/Dropdown.vue
@@ -17,9 +17,28 @@
             }
         },
         methods: {
-            toggleDropdown() {
+            toggleDropdown(e) {
                 this.isActive = !this.isActive;
-            }
+                e.preventDefault();
+                e.stopImmediatePropagation();
+                this.outOfFocus();
+            },
+
+            clickHandler() {
+                this.isActive = false;
+                console.log(this.isActive);
+
+                window.removeEventListener('click', this.clickHandler);
+            },
+
+            outOfFocus() {
+                if(this.isActive) {
+
+                    let _this = this;
+
+                    window.addEventListener('click', this.clickHandler);
+                }
+            },
         },
     }
 </script>
